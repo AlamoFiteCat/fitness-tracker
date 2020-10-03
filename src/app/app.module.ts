591bdc47614d30988c99ba20ca93ee63dd2e53d4
AlamoFiteCat <shredgod_lobos@hotmail.com>
2020-10-03 14:02:01 +0200

    created initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material/material.module';
+import { AppRoutingModule } from './modules/app-routing/app-routing.module';
 
 // [Components]
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { TrainingComponent } from './components/training/training.component';
+import { CurrentTrainingComponent } from './components/current-training/current-training.component';
+import { NewTrainingComponent } from './components/new-training/new-training.component';
+import { PastTrainingsComponent } from './components/past-trainings/past-trainings.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingsComponent,
+    WelcomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/modules/app-routing/app-routing.module.ts
@@ -0,0 +1,25 @@
+// [Modules]
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+// [Components]
+import { WelcomeComponent } from '../../components/welcome/welcome.component';
+import { SignupComponent } from '../../components/auth/signup/signup.component';
+import { LoginComponent } from '../../components/auth/login/login.component';
+import { TrainingComponent } from '../../components/training/training.component';
+
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'training', component: TrainingComponent },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
